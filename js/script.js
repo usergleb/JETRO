ee03b49f27ab7c07a2d280282936050bcaad54b8
lilgleb <lilggleb@gmail.com>
2022-02-16 06:26:44 +0300

    done: first home page + responsive loyout

---
+++ js/script.js
@@ -0,0 +1,24 @@
+new Swiper('.swiper', {
+    // Navigation arrows
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+let header__burger = document.querySelector('.header__burger');
+let header_menu = document.querySelector('.header__menu');
+let back = document.querySelector('body');
+let header__list = document.querySelector('.header__list');
+
+header__burger.onclick = function(){
+    header__burger.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    back.classList.toggle('lock');
+}
+
+header__list.onclick = function () {
+    header__list.classList.remove('active');
+    back.classList.toggle('lock');
+}
